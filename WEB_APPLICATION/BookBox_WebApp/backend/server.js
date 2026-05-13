de71955ecdee75d1155e7ec2d81c8ae12f0e6379
NikD <username@users.noreply.github.com>
2026-05-13 17:14:53 +0200

    made front end and back end start

--- WEB_APPLICATION/BookBox_WebApp/backend/server.js
+++ WEB_APPLICATION/BookBox_WebApp/backend/server.js
@@ -17,7 +17,7 @@ app.get("/api/test", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
+console.log(process.env.MONGO_URI)
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
