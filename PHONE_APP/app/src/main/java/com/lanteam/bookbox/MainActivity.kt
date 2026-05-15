1223438601390944077533aef11480c57f6317e9
JazSemStorz <lan.podlesnik@gmail.com>
2026-05-15 11:22:54 +0200

    added LazyColumn scaffolding to MyBooksScreen
    Introduce a FloatingActionButton to MyBooksScreen and display a list of sample books using LazyColumn (100 items).

--- PHONE_APP/app/src/main/java/com/lanteam/bookbox/MainActivity.kt
+++ PHONE_APP/app/src/main/java/com/lanteam/bookbox/MainActivity.kt
@@ -6,9 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -19,9 +24,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import lanteam.bookbox.ui.theme.MyApplicationTheme
+import androidx.compose.ui.res.stringResource
+import android.widget.Toast
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.platform.LocalContext
 
 enum class AppScreen {
     Map,
@@ -50,6 +60,7 @@ fun BookBoxApp() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
+            val context = LocalContext.current
             CenterAlignedTopAppBar(
                 title = { Text(text = stringResource(R.string.app_name)) }
             )
@@ -119,7 +130,31 @@ fun ListScreen() {
 
 @Composable
 fun MyBooksScreen() {
-    Text("List Screen")
+    val context = LocalContext.current
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                onClick = {
+                    Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+        ) {
+            repeat(100) { index ->
+                item {
+                    Text("Book $index")
+                }
+            }
+        }
+    }
 }
 
 @Composable
